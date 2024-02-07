de9d55c460e5597c3d806aab576f1b15cd7656b9
Brishna Bakshev <b.bakshev@gmail.com>
2024-02-06 20:05:09 -0800

    updated styling and indentation

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
 import os
 
+
 app = Flask(__name__)
 
-img = os.path.join('static', 'images')
+img = os.path.join("static", "images")
 
 @app.route("/")
 def index():
-  file = os.path.join(img, 'img.jpg')
-  return render_template("index.html", img = file, content="Let's tackle one problem at a time!")
-
+    file = os.path.join(img, "img.jpg")
+    return render_template(
+        "index.html", img=file, content="Let's tackle one problem at a time!")
 
 @app.route("/fizzbuzz/<int:num>")
 def fizzBuzz(num):
-  if (num % 3 == 0 and num % 5 == 0):
-    return "FizzBuzz"
-  elif (num % 3 == 0):
-    return "Fizz"
-  elif (num % 5 == 0):
-    print ("Buzz")
-  else:
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
     return str(num)
