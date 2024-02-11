c1765a3230cd98c934051f51e5306b4b5b5fd87f
Brishna Bakshev <b.bakshev@gmail.com>
2024-02-11 12:48:19 -0800

    setting up session

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
 
 app = Flask(__name__)
+# Set secret key
+app.secret_key = "thisIsATest"
 
 img = os.path.join("static", "images")
+file = os.path.join(img, "img.jpg")
 
 @app.route("/")
 def index():
-    file = os.path.join(img, "img.jpg")
+    if "username" not in session:
+        return redirect(url_for("login"))
     return render_template(
-        "index.html", img=file, content="Let's tackle one problem at a time!")
+        "index.html", img=file, content="Let's tackle one problem at a time!"
+    )
 
 user = "admin"
 pwd = "password"
 
-@app.route("/form_login", methods = ["GET", "POST"])
+@app.route("/form_login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+        session["username"] = request.form["username"]
+        session["password"] = request.form["password"]
         invalid_fields = []
-        if username != user:
-            invalid_fields.append({'id': 'username', 'message': 'Username is not valid'})
-        if password != pwd:
-            invalid_fields.append({'id': 'password', 'message': 'Password is not valid'})
+        if session["username"] != user:
+            invalid_fields.append(
+                {"id": "username", "message": "Username is not valid"}
+            )
+        if session["password"] != pwd:
+            invalid_fields.append(
+                {"id": "password", "message": "Password is not valid"}
+            )
         return {
-                "status": "success" if len(invalid_fields) == 0 else "fail",
-                "invalid_fields": invalid_fields
+            "status": "success" if len(invalid_fields) == 0 else "fail",
+            "invalid_fields": invalid_fields,
         }
+    else:
+        redirect(url_for("index"))
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 @app.route("/fizzbuzz/<int:num>")
 def fizzBuzz(num):
@@ -45,5 +61,5 @@ def fizzBuzz(num):
 def youCanViewThis():
     return render_template("youCanView.html")
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True, threaded=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, threaded=True)
