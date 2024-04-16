2517cf4f4da1736497f42eafb60d0e1285367102
Brishna Bakshev <b.bakshev@gmail.com>
2024-04-15 21:22:30 -0700

    user can successfully resend their verification code when the original had expired

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import os
 from models import user
 import hashlib, re, uuid
+from datetime import datetime, timedelta, timezone
 from postmarker.core import PostmarkClient
 
 
@@ -38,8 +39,6 @@ def signup():
         email = request.form["email"]
         token = uuid.uuid4()
         user_account = user.getByUsername(username)
-        # user_id = user.retrieveUserId("user_id")
-        # print("User ID:" + str(user_id))
 
         if len(username) < 4 or len(username) >= 12:
             invalid_fields.append(
@@ -93,10 +92,10 @@ def signup():
             user.createEmailUUID(token, user_id)
             confirm_url = url_for('emailConfirmation', token=token, _external=True)
             html = render_template("confirmation.html", confirm_url=confirm_url)
-            flash("A confirmation email has been sent via email.")
+            flash("A confirmation email has been sent via email.", "success")
 
         #     postmark.emails.send(
-        #     From="pulsarlearning@pulsarlearning.com",
+        #     From="email-signature",
         #     To=email,
         #     Subject="Please confirm your email",
         #     HtmlBody=html,
@@ -112,57 +111,76 @@ def signup():
     if request.method == "GET":
         return render_template("signup.html")
 
+@app.route("/resend-verification-link", methods=["GET", "POST"])
+def resetVerificationLink():
+    if request.method == "POST":
+        email = request.form["email"]
+        print(f"Checking for email: {email}")
+        user_info = user.getByEmail(email)
+        print("First statement", user_info)
+        if user_info is None:
+            print("If statement: User info is None")
+            flash("The email is not on file, click register to signup", "error")
+        elif user_info and user_info[1] == 'false':
+            new_token = str(uuid.uuid4())
+            print(f"User ID: {user_info[0]}, New Token: {new_token}")
+            user.updateEmailUUID(user_info[0], new_token)
+            # confirm_url = url_for('emailConfirmation', new_token=new_token, _external=True)
+            # html = render_template("confirmation.html", confirm_url=confirm_url)
+
+        #     postmark.emails.send(
+        #     From="email-signature",
+        #     To=email,
+        #     Subject="Please confirm your email",
+        #     HtmlBody=html,
+        # )
+            flash("A new email verification has been sent", "success")
+        else:
+            flash("Your email is already verified", "info")
+    return render_template("reset_email_verification.html")
+
 @app.route("/confirmation/<token>", methods=["GET"])
 def emailConfirmation(token):
-    invalid_fields = []
-    user_verification = user.getVerificationCode(token)
+    user_verification = user.confirmVerificationCode(token)
+    print(user_verification)
+    is_verified = user_verification[1]
+    email_uuid_match = user_verification[2] == token
+    time_stamp = user_verification[-1]
 
-    if user_verification:
-        if user_verification[4] != token:
-            invalid_fields.append(
-                    {
-                        "id": "verification", 
-                        "message": "Invalid Code"
-                    }
-                )
-    if len(invalid_fields) == 0:
-        if user_verification is not None:
-            user.isVerified(user_verification[0], True)
-            flash("You are verified")
-            invalid_fields.append(
-                    {
-                        "status": "success",
-                        "message": "You are verified",
-                    }
-                )
-            return redirect(url_for('login'))
-    return render_template("confirmation.html", invalid_fields=invalid_fields)
+    if user_verification is None:
+        flash("Invalid verification link.", "error")
 
-@app.route("/verification_code", methods=["GET", "POST"])
-def emailVerification():
-    if request.method == "POST":
-        invalid_field = []
-        verification = request.form["verification"]
-        user_code = user.getVerificationCode(verification)
+    if datetime.now(timezone.utc) - time_stamp > timedelta(hours=24):
+        flash("Your verification link has expired", "error")
+        return render_template("linkexpired.html")
 
-        if user_code[4] != verification:
-            invalid_field.append(
-                {
-                    "id": "verification", 
-                    "message": "Invalid Code"
-                }
-            )
-        if len(invalid_field) == 0:    
-            user.isVerified(user_code[0], True)
-            invalid_field.append(
-                {
-                    "status": "success",
-                    "message": "You are verified",
-                }
-            )
-            return redirect(url_for('login'))
+    if not is_verified and email_uuid_match:
+        user.isVerified(user_verification[0], True)
+        flash("Verification complete", "success")
+        return redirect(url_for('login'))
+    return render_template("confirmation.html")
 
-    return render_template("verification_code.html")
+#this is form submission version verification
+# @app.route("/verification_code", methods=["GET", "POST"])
+# def emailVerification():
+#     if request.method == "POST":
+#         invalid_field = []
+#         verification = request.form["verification"]
+#         user_code = user.getVerificationCode(verification)
+
+#         if user_code[4] != verification:
+#             invalid_field.append(
+#                 {
+#                     "id": "verification", 
+#                     "message": "Invalid Code"
+#                 }
+#             )
+#         if len(invalid_field) == 0:    
+#             user.isVerified(user_code[0], True)
+            
+#             return redirect(url_for('login'))
+
+#     return render_template("verification_code.html")
 
 @app.route("/form_login", methods=["GET", "POST"])
 def login():
